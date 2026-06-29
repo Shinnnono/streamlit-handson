5e488df07e1add12e09f9f5f92671a9006eb9f52
Shinnnono <shinno2004@i.softbank.jp>
2026-06-29 19:23:59 +0900

    todo.py

--- todo.py
+++ todo.py
@@ -1,24 +1,7 @@
 import streamlit as st
 
-#入力欄とボタン
 st.title("📝 TODOアプリ")
 
-task = st.text_input("タスクを入力")
-
-if st.button("追加"):
-   st.write(f"{task}")
-st.session_state.tasks = []
-
-#step2:リストに入力
-task = st.text_input("タスクを入力")
-if st.button("追加"):
-   tasks.append(task) 
-   st.write(f"{task}")
-
-st.write("### タスク一覧")
-st.write(tasks)
-
-#step3:session_stateを追加
 if "tasks" not in st.session_state:
    st.session_state.tasks = []
 
@@ -27,4 +10,5 @@ if st.button("追加"):
    st.session_state.tasks.append(task)
 
 st.write("### タスク一覧")
-st.write(st.session_state.tasks)
+for i, t in enumerate(st.session_state.tasks):
+   st.checkbox(t, key=i)
